e72456a5ec277bd17b3e699f015314bd26397717
Madhusudhana R K <rkmadhu619@gmail.com>
2018-02-22 18:49:26 +0530

    Backend data is added

--- js/modules/login.js
+++ js/modules/login.js
@@ -6,16 +6,28 @@ $("#submit").on("click", function(){
 		password = $("#password").val(),
 		
 		flag = true;
-		if(email === "" || !ismail(email)){
-			error = "Enter correct Mail Eg: mail@royalsoftwaresolution.com";
-			flag = false;
-		}
-		else if(password === "" ){
-			error = "Please enter password";
-			flag = false;
-		}
-		
-		if(!flag){
-			$(".error").html(error).css({"color":"#FF0000" , "font-weight":"bold" , "text-align": "center" , "margin": "10px 0px"});
-		}
+	if(email === "" || !ismail(email)){
+		error = "Enter correct Mail Eg: mail@royalsoftwaresolution.com";
+		flag = false;
+	}
+	else if(password === "" ){
+		error = "Please enter password";
+		flag = false;
+	}
+	
+	if(!flag){
+		$(".error").html(error).css({"color":"#FF0000" , "font-weight":"bold" , "text-align": "center" , "margin": "10px 0px"});
+	}else{
+		$.ajax({
+			url : "http://royalsoftwaresolution.com/RoyalBackend/data.php",
+			data : {"layout" : 1001, "username" : email , "password" : password},
+			method : "POST",
+			success: function( result){
+				console.log(result);
+			},
+			error : function(error) {
+				console.log(error.responseText);
+			}
+		})
+	}
 })
--- js/modules/signup.js
+++ js/modules/signup.js
@@ -3,11 +3,13 @@ var error = "";
 $("#submit").on("click", function(){
 	$(".error").html("");
 	var name = $("#name").val(),
+		lname = $("#lname").val(),
 		email = $("#email").val(),
 		password = $("#password").val(),
+		gender = $('input[name=Gender]:checked').val();
 		repassword = $("#repassword").val(),
 		flag = true;
-		if((name === "" || name === undefined) || (!isalpha(name))){
+		if((name === "" || name === undefined) || (!isalpha(name)) || !isalpha(lname)){
 			error = "Name Should have only Alphabets";
 			flag = false;
 		}
@@ -19,9 +21,12 @@ $("#submit").on("click", function(){
 			error = "password should be atleast 8 letters with alpha numeric";
 			flag = false;
 		}
-		else if(repassword !== "" ){
-			(password !== repassword) ? error = "Password mismatch" : error = "";
+		else if(repassword === "" ){
+			error = 'Password cannot be blank';
 			flag = false;
+		}
+		else if(repassword !== "" ){
+			(password !== repassword) ? error = "Password mismatch" : error = "";					
 		}else{
 			error = "Enter Re-Password";
 			flag = false;
@@ -29,5 +34,17 @@ $("#submit").on("click", function(){
 
 		if(!flag){
 			$(".error").html(error).css({"color":"#FF0000" , "font-weight":"bold" , "text-align": "center" , "margin": "10px 0px"});
+		}else{
+			$.ajax({
+				url : "http://royalsoftwaresolution.com/RoyalBackend/data.php",
+				data : {"layout" : 1000, "fname" :name ,"lname":lname, "email": email, "password" : password , "gender" : gender},
+				method : "POST",
+				success: function( result){
+					console.log(result);
+				},
+				error : function(error) {
+					console.log(error.responseText);
+				}
+			})
 		}
 })
