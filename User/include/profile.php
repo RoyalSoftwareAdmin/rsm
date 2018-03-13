194d5324af60f3558c8ae5a5ac20b7658c698032
Sneha <sneha@royalsoftwaresolution.com>
2018-03-13 20:34:47 +0530

    today

--- User/include/profile.php
+++ User/include/profile.php
@@ -6,7 +6,7 @@
 				<div class="container profile">
 				    <div class="status">
 					  	<div class="row">
-					 		<div class="col-md-6">
+					 		<div class="col-md-12">
 					  			<h1>Enter Your Details</h1>
 								<h2 id="userStatus"></h2>
 							</div>
@@ -37,7 +37,7 @@
 							      	<p>State</p>
 							      	<select id="stateList"></select>
 							      	<p>Zip</p>
-							      	<input type="text" class="pass zipp" id="zip" maxlength="6" />
+							      	<input type="text" class="pass zipp" id="zip" maxlength="6"class="pn" />
 									<div class="error"></div>
 									<a href="#tab2" class="button outline-inward submit" id="submit">NEXT</a>
 							 	</form class="profile">
@@ -45,7 +45,7 @@
 						</div>
 						<div id="tab2" class="tab-grid">
 							<div class="signin">
-						     	<form class="profile">
+						     	<form class="profile1">
 						     		<p >Organization</p>
 							      	<input id="my-field" type="text" class="user col" >
 						     		<p>Department</p>
@@ -63,7 +63,7 @@
 						<div id="tab3" class="tab-grid">
 							<div class="signin pp">
 						     	<form class="profile">
-								<textarea rows="15" class="comment">
+								<textarea rows="11" class="comment">
 					                 </textarea>
 						     		
 							      
@@ -73,7 +73,7 @@
 						</div>
 						<div id="tab4" class="tab-grid">
 							<div class="signin">
-						     	<form class="profile">
+						     	<form class="profile2">
 
 						     		<p>Personal mobile</p>
 							      	<input type="text" class="user mob" id="mobile" maxlength="10" />
@@ -90,14 +90,14 @@
 						</div>
 						<div id="tab5" class="tab-grid">
 							<div class="signin">
-						     	<form class="profile">
+						     	<form class="profile2">
 						     		<p>Resume
 							      	<input type="file" class="f1"></input><p>
 						     		<p class="p1">Cover Letter <input type="file" class="f1"></input></p>
 							      	
 							      	
 							      	<p class="tet">Other Details
-							      	<textarea  rows="10" col="5" class="comment">
+							      	<textarea  rows="7" col="5" class="comment">
 					                 </textarea></p>					 
 									<a href="#tab1" class="button outline-inward in submit">SUBMIT</a>
 							 	</form class="profile">
