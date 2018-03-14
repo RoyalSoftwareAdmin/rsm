3862ac341c829406eda5b038eff9c28e888997ee
Madhusudhana R K <rkmadhu619@gmail.com>
2018-03-14 17:58:32 +0530

    general

--- User/js/modules/general.js
+++ User/js/modules/general.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
 		 	method: "POST",
 		 	success: function(result){
 				result = JSON.parse(result);
-				$("#name").html(result.Name);
-				$("#email").html(result.EMail);
-				$("#dob").html(result.DateofBirth);
-				$("#loc").html(result.Location);
-				$("#col").html(result.College_Department);
-				$("#tech").html(result.TechnicalSkills);
-				$("#mobnum").html(result.Mobile_Number);
-				$("#Web").html(result.Website);
-				$("#other").html(result.Others);
+				$("#name").html(result.Name == "" ? "N/A" : result.Name);
+				$("#email").html(result.EMail == "" ? "N/A" : result.EMail);
+				$("#dob").html(result.DateofBirth == "" ? "N/A" : result.DateofBirth);
+				$("#loc").html(result.Location == "" ? "N/A" : result.Location);
+				$("#col").html(result.College_Department == " " ? "N/A" : result.College_Department);
+				$("#tech").html(result.TechnicalSkills == "" ? "N/A" : result.TechnicalSkills);
+				$("#mobnum").html(result.Mobile_Number == "" ? "N/A" : result.Mobile_Number);
+				$("#Web").html(result.Website == "" ? "N/A" : result.Website);
+				$("#other").html(result.Others == "" ? "N/A" : result.other);
 		 		console.log(result);
 		 	},
 		 	error : function(result){
