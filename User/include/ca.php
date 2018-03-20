d8535c7f1ea0ded7662675a727d56a22f5eda117
Sneha <sneha@royalsoftwaresolution.com>
2018-03-20 20:18:02 +0530

    today

--- User/include/ca.php
+++ User/include/ca.php
@@ -3,7 +3,54 @@
     <div class="col-md-12 content-top-2 card">
         <div class="agileinfo-cdr">
             <div class="card-header">
-                <h3> For any of the government and private sector general studies are very much important and it brings lot of questions for candidates. One who update daily with the latest politics, worlds , culture , sports and soo on</h3>
+               <div class="container current">
+               	<div class="row">
+               		
+               	 <div class="col-md-4">
+               	 	
+               		 <a href="#"><h4 class="spor">Sports</h4></a>
+               	
+               </div>
+               		<div class="col-md-4">
+               			<a href="#"><h4 class="spor">Science</h4></a>
+               	</div>
+               		<div class="col-md-4">
+               			<a href="#"><h4 class="spor">bussiness</h4></a>
+               		</div>
+               	</div>
+               	</div>
+               
+               
+                 <div class="container current">
+               	<div class="row">
+               		
+               	<div class="col-md-4">
+               		<a href="#"><h4 class="spor">Politics</h4></a>
+               	</div>
+               		<div class="col-md-4">
+               			<a href="#"><h4 class="spor">Awards</h4></a>
+               	</div>
+               		<div class="col-md-4">
+               			<a href="#"><h4 class="spor">International</h4></a>
+               		</div>
+               	</div>
+               	</div>
+               
+               <div class="container current">
+               	<div class="row">
+               	<div class="col-md-4">
+               		<a href="#"><h4 class="spor">General</h4></a>
+               	</div>
+               		<div class="col-md-4">
+               			<a href="#"><h4 class="spor">Economy</h4></a>
+               	</div>
+               		<div class="col-md-4">
+               			<a href="#"><h4 class="spor">ALL</h4></a>
+               		</div>
+               	</div>
+               </div>
+               
+
             </div>
         </div>
         </div>
