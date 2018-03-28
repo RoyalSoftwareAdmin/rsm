24551e9747b18e492aa3a02c9f334ca79325e61d
Saba <saba@royalsoftwaresolution.com>
2018-03-28 11:05:40 +0530

    analytical.php

--- User/include/analytical.php
+++ User/include/analytical.php
@@ -48,17 +48,4 @@
 		
  </div>      
     <div class="clearfix"> </div>
-</div>
-<script src="js/modules/analytical.js"></script>  <link rel="stylesheet" href="css/modules/analytical.css">
- <div class="row-one widgettable">
-    <div class="col-md-12 content-top-2 card">
-        <div class="agileinfo-cdr">
-            <div class="card-header">
-                <h3> Analytical Skills are basic need for any startup. When a project starts, one should analyze the probablity of acheiving it in all possible solution and bring the best of it.</h3>
-            </div>
-        </div>
-        </div>
-    </div>      
-    <div class="clearfix"> </div>
-</div>
-<script src="js/modules/analytical.js"></script>  
+<script src="js/modules/analytical.js"></script> 
