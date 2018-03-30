7dcfe248954aeb3994ad183de485446e003e5f6c
Sneha <sneha@royalsoftwaresolution.com>
2018-03-30 16:30:57 +0530

    today

--- Admin/js/modules/index.js
+++ Admin/js/modules/index.js
@@ -1,14 +1,46 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $.ajax({
         url: '../apis/AdminData.php',
         type: 'POST',
         data : {"layout" : "1006"},
         success: function (data) {
             data = JSON.parse(data);
-           console.log(data);
+
+            $("#users").Plugin_Table({
+                resultData: data,
+                datatablerequired: true,
+                ClickColumns: "4"
+            })
+            $(".btn.btn-success.btn-xs.pull-right").html(result.tableData["0"].Category.length)
         },
-        error : function(error){
+           /*console.log(data);*/
+        
+       /*error : function(error){
             console.log(error);
         }
     });   
+})  
+*/
+
+$(document).ready(function(){
+    $.ajax({
+        url : "../apis/AdminData.php",
+        data : {"layout" : "1006"},
+        method: "POST",
+        success: function(result){
+        result = JSON.parse(result);
+            $("#users").Plugin_Table({
+                resultData: result,
+                datatablerequired: true,
+                ClickColumns: "4"
+            })
+           /* $(".btn.btn-success.btn-xs.pull-right").html(result.tableData["0"].user.length)*/
+        },
+        error : function(result){
+            console.log(result);
+        }
+    })
 })
+function redirecttopage(jsonValue) {
+    var SelectedData = jsonValue;   
+}
